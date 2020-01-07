5920173c712181d59723aec0cee6e8fb7e25067b
Naveed Naqi <nnaqi5534@gmail.com>
2020-01-07 11:42:30 -0500

    Removed un-needed imports

--- src/components/SingleContactCard.js
+++ src/components/SingleContactCard.js
@@ -2,9 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
-import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
-import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 
 const useStyles = makeStyles({
@@ -26,7 +24,6 @@ const useStyles = makeStyles({
 
 export default function SingleContactCard({name, phoneNumber, email}) {
   const classes = useStyles();
-  const bull = <span className={classes.bullet}>•</span>;
 
   return (
     <Card className={classes.card} variant="outlined">
